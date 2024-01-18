f2abcf12510aac04a4079863003a6b75db25f8e7
Lichyo <lichyo003@gmail.com>
2024-01-18 11:28:21 +0800

    solve conflic

--- src/R2/auto.py
+++ src/R2/auto.py
@@ -12,8 +12,8 @@ limit = DigitalIn(brain.three_wire_port.d)
 ir = DigitalIn(brain.three_wire_port.a)
 
 controller = Controller()
-driver.set_drive_velocity(10, PERCENT)
-
+driver.set_drive_velocity(70, PERCENT)
+roller = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False)
 vision_1__SIG_1 = Signature(1, -4789, -4193, -4492,-5041, -4557, -4800,3.9, 0)
 vision = Vision(Ports.PORT1, 50, vision_1__SIG_1)
 stretch = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
@@ -23,13 +23,18 @@ throw= MotorGroup(throw_1, throw_2)
 arm = Motor(Ports.PORT15, GearSetting.RATIO_36_1, False)
 arm.set_timeout(1,SECONDS)
 throw.set_stopping(HOLD)
-throw.set_velocity(40, PERCENT)
+throw.set_velocity(60, PERCENT)
 throw.set_max_torque(100, PERCENT)
-stretch.set_velocity(80, PERCENT)
+stretch.set_velocity(100, PERCENT)
 stretch.set_stopping(COAST)
 stretch.set_timeout(1, SECONDS)
 throw.set_timeout(1, SECONDS)
+roller.set_velocity(80, PERCENT)
 is_strethced = False
+global auto
+global counter
+auto = True
+counter = 0
 
 class Accepter:
     def __init__(self):
@@ -37,15 +42,17 @@ class Accepter:
         self.is_stretching = False
     
     def throw_prepare(self,ir):
+        throw.set_velocity(80, PERCENT)
         while ir.value() == 1: 
             throw.spin(FORWARD)
         throw.stop()
+
     def stretch_prepare(self):
         if self.is_stretching:
             pass
         else:
             self.is_stretching = True
-            stretch.spin_for(FORWARD, 470, DEGREES)
+            stretch.spin_for(FORWARD, 450, DEGREES)
     
     def prepare(self,ir):
         if self.is_stretching:
@@ -60,39 +67,123 @@ class Accepter:
         
             
     def execute_preload(self): 
-        Thread(lambda: arm.spin_for(FORWARD,180,DEGREES) )
+        arm.spin_for(FORWARD,130,DEGREES)
+        arm.set_stopping(HOLD)
+        arm.stop()
         self.prepare(ir)
-        arm.spin_for(REVERSE,180,DEGREES)
-        
+        arm.spin_for(REVERSE,130,DEGREES)
 
     def set_stop(self):
         throw.stop()
         stretch.stop()
 
-    def shoot(self):
+    def shoot(self, counter):
         if self.prepared:
-            stretch.spin_for(REVERSE, 470, DEGREES)
+            throw.set_velocity(60, PERCENT)
+            stretch.spin_for(REVERSE, 450, DEGREES)
             throw.spin_for(FORWARD,50,DEGREES)
             self.prepared = False
             self.is_stretching = False
+            return counter +1
         else:
             self.set_stop()
-    
-    
-counter = 0
+
+def switch_to_auto():
+    global auto
+    global counter
+    while True:
+        if controller.buttonX.pressing():
+            auto = not auto
+            counter = 0
+            wait(0.2, SECONDS)
+            brain.screen.print("change")
+
+thread = Thread(switch_to_auto)
+
 accepter = Accepter()
 accepter.execute_preload()
-while True and counter < 23:
-    if not accepter.prepared:
-        accepter.prepare(ir)
-    else:
-        objects = vision.take_snapshot(vision_1__SIG_1)
-        if objects == None:
-            pass
+while True:
+    if counter == 23:
+        auto = False
+    if auto:
+        if not accepter.prepared:
+            accepter.prepare(ir)
         else:
-            objects = vision.largest_object()
-            if objects.height > 30 and objects.width > 50:
-                accepter.shoot()
-                counter = counter + 1
+            objects = vision.take_snapshot(vision_1__SIG_1)
+            if objects == None:
+                pass
+            else:
+                objects = vision.largest_object()
+                if objects.height > 10 and objects.width > 20:
+                    counter = accepter.shoot(counter)
+    else:
+        if controller.buttonDown.pressing():
+            stretch.spin(FORWARD)
+        elif controller.buttonUp.pressing():
+            stretch.spin(REVERSE)
+        elif controller.buttonB.pressing():
+            stretch.spin(REVERSE)
+        else:
+            stretch.stop()
+                    
+        if controller.buttonA.pressing():
+            throw.spin(FORWARD)
+        else:
+            throw.stop()
 
-driver.drive_for(FORWARD, 2000, MM)
+        if controller.buttonR1.pressing():
+            roller.spin(FORWARD)
+        else:
+            roller.stop()
+
+        if controller.buttonL1.pressing():
+            arm.spin_for(FORWARD,130,DEGREES)
+            arm.set_stopping(HOLD)
+            arm.stop()
+        elif controller.buttonL2.pressing():
+            arm.spin_for(REVERSE,130,DEGREES)
+        else:
+            arm.stop
+
+        left_velocity = 0
+        right_velocity = 0
+        v = controller.axis3.position()
+        h = controller.axis4.position()
+        right_axis_h = controller.axis1.position()
+
+        left_velocity = abs(v)
+        right_velocity = abs(v)
+
+        if right_axis_h > 0:
+            if v >= 0:
+                left_velocity += abs(right_axis_h)
+            if v < 0:
+                right_velocity += abs(right_axis_h) 
+        elif right_axis_h < 0:
+            if v >= 0:
+                right_velocity += abs(right_axis_h) 
+            if v < 0:
+                left_velocity += abs(right_axis_h)
+        elif h > 0:
+            left_velocity += abs(h)
+        else:
+            right_velocity += abs(h)    
+
+        if v >= 10:
+            left_wheels.spin(FORWARD,left_velocity, RPM)
+            right_wheels.spin(FORWARD,right_velocity, RPM)
+        elif v < -10:
+            left_velocity = round(0.8 * left_velocity)
+            right_velocity = round(0.8 * right_velocity)
+            left_wheels.spin(REVERSE,left_velocity, RPM)
+            right_wheels.spin(REVERSE,right_velocity, RPM)
+        else:           
+            if h < 0 or right_axis_h < 0:
+                left_wheels.spin(REVERSE,right_velocity, RPM)
+                right_wheels.spin(FORWARD,right_velocity, RPM)
+            elif h > 0 or right_axis_h > 0:
+                right_wheels.spin(REVERSE,left_velocity, RPM)
+                left_wheels.spin(FORWARD,left_velocity, RPM)
+            else:
+                right_wheels.stop()
+                left_wheels.stop()
